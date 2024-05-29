6a2562d7dbaa16a72d60f01927d7c9102583149d
drakenevadie19 <drakennievadje19@gmail.com>
2024-05-29 16:52:15 -0500

    New Change: Adjust display profiles div

--- front-end/src/components/profile-list.js
+++ front-end/src/components/profile-list.js
@@ -2,18 +2,21 @@ const ProfileList = ({ profiles }) => {
 
     return (
         <>
-            <div className="display-profiles">
+            <div className="display-profiles-frame">
                 {profiles && profiles.length>0 
-                    ? profiles.map((github) => (
-                        <div class="card">
-                            <img src={github.avatar_url} class="card-img-top" alt="github" />
-                            <div class="card-body">
-                                <h5 class="card-title">{github.login}</h5>
-                                {/* <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
-                                <a href={github.html_url} class="btn btn-primary" target="_blank" rel="noreferrer">View Profile</a>
+                    ? 
+                    <div className="display-profiles">
+                        {profiles.map((github, key) => (
+                            <div class="card" key={key}>
+                                <img src={github.avatar_url} class="card-img-top" alt="github" />
+                                <div class="card-body">
+                                    <h5 class="card-title">{github.login}</h5>
+                                    {/* <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+                                    <a href={github.html_url} class="btn btn-primary"  target="_blank" rel="noreferrer">View Profile</a>
+                                </div>
                             </div>
-                        </div>
-                    ))
+                        ))}
+                    </div>
                     : <h4><i>No match Github profiles to display</i></h4>    
                 }
             </div>
