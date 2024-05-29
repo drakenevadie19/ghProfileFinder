8896edd4665eb0bf922bf982bb0da9f2b076a75f
drakenevadie19 <drakennievadje19@gmail.com>
2024-05-29 18:42:54 -0500

    New Change: Add functions for searching by users' names

--- front-end/src/pages/find-by-name.js
+++ front-end/src/pages/find-by-name.js
@@ -1,21 +1,19 @@
 import { useNavigate } from "react-router-dom";
 import { useEffect, useRef, useState } from "react";
 import ProfileList from "../components/profile-list";
+import axios from 'axios';
 
 const FindByName = () => {
     const [nameQuery, setNameQuery] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [profiles, setProfiles] = useState([]);
     
     const [aboveDivHeight, setAboveDivHeight] = useState(0);
     const aboveDivRef = useRef(null);
 
-
-    const handleSearch = (nQuery) => {
-        alert(nQuery);
-    }
-
     const navigate = useNavigate();
 
-
+    // setting dynamically height of profiles displaying div
     const updateHeight = () => {
     const height = aboveDivRef.current.offsetHeight;
         setAboveDivHeight(height);
@@ -33,22 +31,55 @@ const FindByName = () => {
         height: `calc(100% - ${aboveDivHeight}px)`,
     };
 
+    // Handling fetch API
+    const handleSearch = async () => {
+        setLoading(true);
+        try {
+            const response = await axios.get('https://api.github.com/search/users', {
+                params: { q: `${nameQuery} in:name` },
+                headers: { 'Accept': 'application/vnd.github.v3+json' },
+            });
+            setProfiles(response.data.items);
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setLoading(false);
+        }
+    }
+    console.log(profiles);
+
     return (
     <>
         <div className="find-frame" id="aboveDiv" ref={aboveDivRef}>
-            <button
-                type="button"
-                className="btn btn-primary"
-                onClick={() => navigate("/")}
-            >
-                Back to Home
-            </button>
+        <div className="navigation-buttons-group">
+                <button
+                    type="button"
+                    className="btn btn-primary"
+                    onClick={() => navigate("/")}
+                >
+                    Back to Home
+                </button>
+                {profiles.length>0 &&
+                    <button
+                        type="button"
+                        className="btn btn-primary"
+                        onClick={() => {
+                            setProfiles([]);
+                            setNameQuery("")
+                        }}
+                    >
+                        Clear
+                    </button>
+                }
+            </div>
+
             <h2>Type in name of user you want to find to search:</h2>
             <div class="input-group mb-3">
                 <input
                 type="text"
                 class="form-control"
                 placeholder="E.g: Thanh Nguyen Do"
+                value={nameQuery}
                 aria-label="Recipient's name"
                 aria-describedby="button-addon2"
                 onChange={(e) => setNameQuery(e.target.value)}
@@ -57,7 +88,7 @@ const FindByName = () => {
                 class="btn btn-primary" 
                 type="button" 
                 id="button-addon2"
-                onClick={() => handleSearch(nameQuery)}
+                onClick={() => handleSearch()}
                 >
                 Search
                 </button>
@@ -65,7 +96,11 @@ const FindByName = () => {
         </div>
 
         <div className="profiles-display" id="belowDiv" style={{ ...belowDivStyle }}>
-            <ProfileList />
+        {
+                loading
+                ? <h4>Loading...</h4>
+                :<ProfileList profiles={profiles} />
+            }
         </div>
     </>
     );
